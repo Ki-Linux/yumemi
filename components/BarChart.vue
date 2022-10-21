7e17d4e8aef8b891c7f16f5268d73a93e4e5b128
Ki-Linux <linuxseima@gmail.com>
2022-10-21 19:24:13 +0900

    chart.js　追加

---
+++ components/BarChart.vue
@@ -0,0 +1,16 @@
+<script>
+import { Line } from 'vue-chartjs'
+
+export default {
+  extends: Line,
+  data() {
+    return {
+      chartData: {},
+      options: {},
+    }
+  },
+  mounted() {
+    this.renderChart(this.chartData, this.options)
+  },
+}
+</script>
--- pages/PopulationGraph.vue
+++ pages/PopulationGraph.vue
@@ -2,16 +2,19 @@
     <div>
         <top_title/>
         <table_list @toGraph="graphShow"/>
+        <chart />
     </div>
 </template>
 <script lang="ts">
 import { Vue, Component } from 'nuxt-property-decorator';
 import TopTitle from '@/components/TopTitle.vue';
 import TableList from '@/components/TableList.vue';
+import Chart from '@/components/BarChart.vue';
 @Component({
     components: {
         'top_title': TopTitle,
         'table_list': TableList,
+        Chart,
     }
 })
 export default class PoplulationGraph extends Vue{
