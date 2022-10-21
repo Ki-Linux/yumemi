35e94bb9acb758e29a5d1e98dfc3ab43ebb1a90b
Ki-Linux <linuxseima@gmail.com>
2022-10-21 14:21:20 +0900

    reset.cssとタイトルを追加

---
+++ components/TopTitle.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1 style="
+            backgroundColor: #C0C0C0; 
+            font-size: 30px;
+            padding: 30px 0;
+            text-align: center;"
+        >
+        都道府県別の総人口推移グラフ
+        </h1>
+    </div>
+</template>
---
+++ pages/PopulationGraph.vue
@@ -0,0 +1,15 @@
+<template>
+    <top_title/>
+</template>
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator';
+import TopTitle from '@/components/TopTitle.vue';
+@Component({
+    components: {
+        'top_title': TopTitle,
+    }
+})
+export default class PoplulationGraph extends Vue{
+    i: string = "io";
+}
+</script>
