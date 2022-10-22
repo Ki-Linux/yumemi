32c177aadb3aa28247a12b00e5032e525706f5ce
Ki-Linux <linuxseima@gmail.com>
2022-10-22 20:45:33 +0900

    github pages用にリポジトリパスを追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,7 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/Ki-Linux/favicon.ico' }
     ]
   },
 
@@ -59,4 +59,8 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
   },
+
+  router: {
+    base: '/Ki-Linux/'
+  }
 }
